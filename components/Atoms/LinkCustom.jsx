d22e16ead2ced0329d68f65c08745deae3034a85
@Wil <emma.wilinston@gmail.com>
2021-04-08 17:50:17 -0500

    se crea LinkCustom con la funcionalidad de poner el estado loading en true

---
+++ components/Atoms/LinkCustom.jsx
@@ -0,0 +1,21 @@
+import { connect } from 'react-redux'
+import { loadingTrue } from '../../redux/actionCreator'
+
+const LinkCustom = ({ text, loadingTrue }) => {
+  const loading = () => {
+    loadingTrue()
+  }
+  return <span onClick={loading}>{text}</span>
+}
+
+const mapStateToProps = () => {}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    loadingTrue() {
+      dispatch(loadingTrue())
+    },
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(LinkCustom)
