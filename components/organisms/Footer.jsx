ea81cc394d68d292f1cbf8077725b4f73f7fd53f
@Wil <emma.wilinston@gmail.com>
2021-04-21 10:19:51 -0500

    se pone en 2 parrafos el contenido del footer

--- components/organisms/Footer.jsx
+++ components/organisms/Footer.jsx
@@ -1,16 +1,16 @@
 const Footer = () => {
   return (
-    <div>
-      <footer className="footer">
-        <div className="content has-text-centered">
-          <p>
-            ¡Encuentra todo lo que buscas en Goshop.com, nuestra Tienda Online!
-            <strong> GoshopPro</strong> todos los
-            <a href="#"> Derechos</a> y recursos estan reservados
-          </p>
-        </div>
-      </footer>
-    </div>
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <p>
+          ¡Encuentra todo lo que buscas en Goshop.com, nuestra Tienda Online!
+        </p>
+        <p>
+          <strong> GoshopPro</strong> todos los
+          <a href="#"> Derechos</a> y recursos estan reservados
+        </p>
+      </div>
+    </footer>
   )
 }
 
