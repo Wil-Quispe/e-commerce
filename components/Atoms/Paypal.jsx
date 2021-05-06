e5713c925b4fd9c6d7aebc7ad5c80463c8d78d6a
@Wil <emma.wilinston@gmail.com>
2021-05-06 10:09:40 -0500

    se comvierte en un hook para poder reutilizar el codigo y para devolver datos al componente padre y se quiita el useRef porque como hook dava algunos errores

--- components/Atoms/Paypal.jsx
+++ components/Atoms/Paypal.jsx
@@ -1,41 +1,49 @@
-import { useRef, useEffect } from 'react'
+import { Row } from 'antd'
+import { useState } from 'react'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const Paypal = () => {
-  const paypal = useRef()
+const Paypal = ({ description, amount }) => {
+  const PayPalButton = paypal.Buttons.driver('react', { React, ReactDOM })
+  const [dataPay, setDataPay] = useState()
 
-  useEffect(() => {
-    window.paypal
-      .Buttons({
-        createOrder: (data, actions, err) => {
-          return actions.order.create({
-            intent: 'CAPTURE',
-            purchase_units: [
-              {
-                description: 'cool table',
-                amount: {
-                  currency_code: 'USD',
-                  value: 100,
-                },
-              },
-            ],
-          })
+  const createOrder = (data, actions, err) => {
+    return actions.order.create({
+      intent: 'CAPTURE',
+      purchase_units: [
+        {
+          description,
+          amount: {
+            currency_code: 'USD',
+            value: amount,
+          },
         },
-        onApprove: async (data, actions) => {
-          const order = await actions.order.capture()
-          console.log(order)
-        },
-        onError: err => {
-          console.log(err)
-        },
-      })
-      .render(paypal.current)
-  }, [])
+      ],
+    })
+  }
+  const onApprove = async (data, actions) => {
+    const order = await actions.order.capture()
+    setDataPay({ status: 'ok' })
+  }
+  const onError = err => {
+    console.log(err)
+    setDataPay({ status: 'error' })
+  }
 
-  return (
-    <div>
-      <div ref={paypal}></div>
-    </div>
+  const PayPal = () => (
+    <Row justify="center">
+      <PayPalButton
+        createOrder={(data, actions) => createOrder(data, actions)}
+        onApprove={(data, actions) => onApprove(data, actions)}
+        onError={err => onError(err)}
+      />
+    </Row>
   )
+
+  return {
+    PayPal,
+    dataPay,
+  }
 }
 
 export default Paypal
