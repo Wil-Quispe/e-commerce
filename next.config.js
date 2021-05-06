eaa25aabbaf359f3e16052443c79f991e9370d5d
@Wil <emma.wilinston@gmail.com>
2021-05-06 10:26:32 -0500

    logica para la llamada a la API y saber si se ejecuta local o production

--- next.config.js
+++ next.config.js
@@ -1,10 +1,23 @@
-module.exports = {
-  env: {
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+
+module.exports = phase => {
+  if (PHASE_DEVELOPMENT_SERVER === phase) {
+    const env = {
+      STRIPE:
+        'pk_test_51IOpquKXTg6hXZjZIbyjoZNlRqSSaHNYwJKcS8Mocszzzfeg99KTLcJklXdjD4l3pCV1W5sIBTphJb3JotsjBtbR000wub94Dn',
+      URI: 'http://localhost:5000',
+      SITE_NAME: 'Goshop',
+      ADMIN_ID: '607f51a529a0b668d8f0d0e9',
+    }
+    return { env }
+  }
+  const env = {
     STRIPE:
       'pk_test_51IOpquKXTg6hXZjZIbyjoZNlRqSSaHNYwJKcS8Mocszzzfeg99KTLcJklXdjD4l3pCV1W5sIBTphJb3JotsjBtbR000wub94Dn',
-    // URI: 'https://e-back.vercel.app',
-    URI: 'http://localhost:5000',
+    URI: 'https://e-back.vercel.app',
     SITE_NAME: 'Goshop',
-    ADMIN_ID: '607b5f4d0b459701c82f5639',
-  },
+    ADMIN_ID: '607f51a529a0b668d8f0d0e9',
+  }
+
+  return { env }
 }
