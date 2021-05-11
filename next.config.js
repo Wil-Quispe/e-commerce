2dddec0ec864924b1aae4fb26a25fefa57bf55c8
@Wil <emma.wilinston@gmail.com>
2021-05-11 14:29:20 -0500

    optimizacion de variables de entorno

--- next.config.js
+++ next.config.js
@@ -1,23 +1,16 @@
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 
 module.exports = phase => {
-  if (PHASE_DEVELOPMENT_SERVER === phase) {
-    const env = {
-      STRIPE:
-        'pk_test_51IOpquKXTg6hXZjZIbyjoZNlRqSSaHNYwJKcS8Mocszzzfeg99KTLcJklXdjD4l3pCV1W5sIBTphJb3JotsjBtbR000wub94Dn',
-      URI: 'http://localhost:5000',
-      SITE_NAME: 'Goshop',
-      ADMIN_ID: '607f51a529a0b668d8f0d0e9',
-    }
-    return { env }
-  }
   const env = {
-    STRIPE:
-      'pk_test_51IOpquKXTg6hXZjZIbyjoZNlRqSSaHNYwJKcS8Mocszzzfeg99KTLcJklXdjD4l3pCV1W5sIBTphJb3JotsjBtbR000wub94Dn',
-    URI: 'https://e-back.vercel.app',
+    URI: '',
     SITE_NAME: 'Goshop',
     ADMIN_ID: '607f51a529a0b668d8f0d0e9',
   }
+  if (PHASE_DEVELOPMENT_SERVER === phase) {
+    env.URI = 'http://localhost:5000'
+    return { env }
+  }
+  env.URI = 'https://e-back.vercel.app'
 
   return { env }
 }
