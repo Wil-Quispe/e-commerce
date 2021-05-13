36fc99ecf8067bc8b874fca86cdf8f9357274344
@Wil <emma.wilinston@gmail.com>
2021-05-13 16:00:21 -0500

    despues del pago en peru que hace

--- components/Atoms/PayPeru.jsx
+++ components/Atoms/PayPeru.jsx
@@ -1,6 +1,9 @@
-import { Button, Col, Image, Row } from 'antd'
+import { Button, Col, Image, Popconfirm, Row } from 'antd'
 
 const PayPeru = ({ amount, img, qr, color }) => {
+  const confirmPay = () => {
+    location.href = '/pago/exitoso'
+  }
   return (
     <div>
       <Row justify="center">
@@ -21,7 +24,7 @@ const PayPeru = ({ amount, img, qr, color }) => {
           <h3 className="name_color_pay">Wil Quispe</h3>
           <h3 className="number_pay">923 383 923</h3>
           <h3>Total a pagar</h3>
-          <h3>S/{amount}.00</h3>
+          <h3>S/{amount}</h3>
 
           <h3>1. Ingrese el monto total del pedido y realice el pago.</h3>
           <h3>
@@ -32,7 +35,14 @@ const PayPeru = ({ amount, img, qr, color }) => {
           </h3>
           <h3>3. Verificaremos su pago y completaremos su pedido en breve.</h3>
           <Row>
-            <Button type="primary">Ya pagué siguiente paso</Button>
+            <Popconfirm
+              title="Acepte si en verdad ya pagaste"
+              onConfirm={confirmPay}
+              okText="Si Ya pagué"
+              cancelText="Todavía no pago"
+            >
+              <Button type="primary">Ya pagué siguiente paso</Button>
+            </Popconfirm>
           </Row>
         </Col>
       </Row>
